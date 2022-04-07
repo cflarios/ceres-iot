475601c63c5b8f2d42c4a2a49bf562e4d9079763
Julian Vivas <87610014+JulCode@users.noreply.github.com>
2022-04-07 17:12:19 -0500

    Initial commit
    elementos web

---
+++ web_code/js/paint.js
@@ -0,0 +1,165 @@
+
+const temp = document.getElementById("tempGrafic").getContext("2d");
+const soil = document.getElementById("humSoil").getContext("2d");
+const water1 =  document.getElementById("cylGauge1").getContext("2d")
+const water2 =  document.getElementById("cylGauge2").getContext("2d")
+var gradientStroke = temp.createLinearGradient(0, 230, 0, 50);
+gradientStroke.addColorStop(1, "rgba(66,134,121,0.15)");
+gradientStroke.addColorStop(0.4, "rgba(66,134,121,0.0)"); //green colors
+gradientStroke.addColorStop(0, "rgba(66,134,121,0)"); //green colors
+
+
+const tankWater1 = new Chart(water1,{
+  type: "bar",  
+  data :{
+    
+    labels: [''],
+    datasets: [
+      {
+        label: 'Level de agua',
+        borderColor: styles.color.alphas[2],
+        backgroundColor: styles.color.solids[1],
+        data:[],
+        borderWidth:1,
+        categoryPercentage: 0.77
+    },
+          
+    ]
+}, options: {
+    maintainAspectRatio: false,
+    scaleFontColor: '#fff',
+    scales: {
+        x: {          
+            stacked:true,    
+        },
+        y:{
+          suggestedMax: 100,
+        }
+    }
+}
+})
+const tankWater2 = new Chart(water2,{
+  type: "bar",  
+  data :{
+    
+    labels: [''],
+    datasets: [
+      {
+        label: 'Level de agua',
+        borderColor: styles.color.alphas[2],
+        backgroundColor: styles.color.solids[1],
+        data:[40],
+        borderWidth:1,
+        categoryPercentage: 0.77
+    },
+          
+    ]
+}, options: {
+    maintainAspectRatio: false,
+    scaleFontColor: '#fff',
+    scales: {
+        x: {          
+            stacked:true,    
+        },
+        y:{
+          suggestedMax: 100,
+        }
+    }
+}
+
+
+})
+
+const humiditySoil = new Chart(soil, {
+  type: "doughnut",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "humedad %",
+        backgroundColor: ["green",gradientStroke],
+        borderColor: "#00d6b4",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        data: [],
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        //barPercentage: 0.5,
+        /* suggestedMin: 50,
+        suggestedMax: 55, */
+        ticks: {
+          display: true,
+        },
+      },
+      x: {
+        //suggestedMin: 0,
+        //suggestedMax: 55,
+        ticks: {
+
+          display: true,
+        },
+      },
+    },
+    legend: {
+      display: true
+    },
+    circumference: 180,
+    rotation: -90,
+    cutout: 90,
+  },
+});
+
+const temperatureGrafic = new Chart(temp, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Temperatura Cº",
+        data: [],
+        //backgroundColor: styles.color.solids[3],
+        //borderColor: styles.color.solids[5],
+        backgroundColor: gradientStroke,
+        borderColor: "#00d6b4",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: "#00d6b4",
+        pointBorderColor: "rgba(255,255,255,0)",
+        pointHoverBackgroundColor: "#00d6b4",
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+      },
+    ],
+  },
+  options: {
+    legend: {
+      display: true
+    },
+    maintainAspectRatio: false,
+    scaleFontColor: "#fff",
+    scales: {
+      y:{
+        /* suggestedMin: 50,
+        suggestedMax: 55, */
+        ticks: {
+          display: true,
+        },
+      },
+      x:{
+        suggestedMin: 0,
+        suggestedMax: 55,
+        ticks: {
+          display: true,
+        },
+      },
+    },
+  },
+});
---
+++ web_code/server/sketch.js
@@ -0,0 +1,60 @@
+
+// Creado ChepeCarlos de ALSW
+// Tutorial Completo en https://nocheprogramacion.com
+// Canal Youtube https://youtube.com/alswnet?sub_confirmation=1
+
+//const mqtt = require("mqtt")
+var client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt");
+//Si lo usas en hosting para usar encriptado https
+//var client = mqtt.connect("wss://test.mosquitto.org:8081/mqtts")
+
+function EventoConectar() {
+  console.log("Conectado a MQTT");
+  client.subscribe("esp/#", function (err) {
+    if (!err) {
+      //client.publish("ALSW/Temperatura", "30");
+      document.body.classList.add("running");
+      console.log("good");
+    }
+  });
+}
+let uno = 0;
+let dos = 0;
+let tres=0;
+let counters = 0;
+function EventoMensaje(topic, message) {
+  if (topic == "esp/dht/temperature") {
+    console.log("La Temperatura es " + message.toString());
+    temperatureGrafic.data.labels.push(counters);
+    temperatureGrafic.data.datasets.forEach((dataset) => {
+      dataset.data.push(message.toString());
+    });
+    counters++;
+    temperatureGrafic.update();
+  }
+  if (topic == "esp/dht/humidity/innen" || topic == "esp/dht/humidity" ) {
+   // console.log("La humedad es " + message.toString());
+    uno = message;
+    dos = 100 - message;
+    /*humiditySoil.data.datasets.data.push(uno,dos);*/
+    humiditySoil.data.datasets.forEach((dataset) => {
+      dataset.data = [uno,dos];
+    });
+    humiditySoil.update();
+  }
+  if (topic == "esp/dht/humidity") {
+    console.log("La uwu " + message.toString());
+
+    tankWater1.data.datasets.forEach((dataset) => {
+     dataset.data = [message.toString()];
+    }); 
+    
+    tankWater1.update();
+  }
+  //console.log(topic + " - " + message.toString());
+}
+client.on("connect", EventoConectar);
+client.on("message", EventoMensaje);
+//client.on("message", tal);
+
+console.log("Empezando a conectar");
