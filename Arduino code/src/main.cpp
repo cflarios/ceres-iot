61e33c45f23632c4f8f6da9f0fa173e6d63bc503
Chris Larios <102568344+Sirius-py@users.noreply.github.com>
2022-04-16 13:45:46 -0500

    Actualización

--- Arduino code/src/main.cpp
+++ Arduino code/src/main.cpp
@@ -24,18 +24,16 @@ const int mqtt_port = 1884;
 // Tópicos que publica
 const char *topic1 = "ceres/sensor/ambiente/temperatura";
 const char *topic2 = "ceres/sensor/ambiente/humedad";
-const char *topic3 = "ceres/sensor/distancia"; // Tópico de prueba, no irá en la versión beta de Ceres.
+const char *topic3 = "ceres/sensor/distancia";
 const char *topic6 = "ceres/sensor/planta/humedad-tierra";
-//const char *topic7 = "ceres/tanque/principal"; ---> Ignorar este topico
-//const char *topic8 = "ceres/tanque/auxiliar"; ----> Este también ignoralo
+const char *topic7 = "ceres/tanque/principal/estado";              // El estado del tanque principal es (...)
+const char *topic8 = "ceres/tanque/auxiliar/estado";               // El estado del tanque auxiliar es (...)
 const char *topic9 = "ceres/tanque/principal/volumen-total";       // La cantidad que soporta el tanque principal es (...) litros
 const char *topic10 = "ceres/tanque/auxiliar/volumen-total";       // La cantidad que soporta el tanque auxiliar es (...) litros
 const char *topic11 = "ceres/tanque/principal/volumen-liquido";    // La cantidad de liquido faltante en el interior del tanque principal es: (...) litros
 const char *topic12 = "ceres/tanque/auxiliar/volumen-liquido";     // La cantidad de liquido faltante en el interior del tanque auxiliar es: (...) litros
 const char *topic13 = "ceres/tanque/principal/porcentaje-liquido"; // El porcentaje que ocupa el liquido en el tanque principal es: (...) %
 const char *topic14 = "ceres/tanque/auxiliar/porcentaje-liquido";  // El porcentaje que ocupa el liquido en el tanque auxiliar es: (...) %
-const char *topic15 = "ceres/tanque/principal/estado";             // El estado del tanque principal es (...)
-const char *topic16 = "ceres/tanque/auxiliar/estado";              // El estado del tanque auxiliar es (...)
 
 // Tópicos a los que se suscribe
 const char *topic4 = "ceres/led";
@@ -62,8 +60,8 @@ const int resolution = 8;
 String slider_value = "0";
 
 //-------------Bombas------------------//
-const int Bomba_Entrada = 15;
-const int Bomba_Salida = 4;
+const int Bomba_Entrada = 15; // Bomba del tanque auxiliar
+const int Bomba_Salida = 4;   // Bomba del tanque principal
 
 // Pin indicadores Tanque Principal
 const int led_low_one = 12;
@@ -83,7 +81,8 @@ float nl_one;
 float c_one;
 float NTc_one;
 float NTl_one;
-// DAtos de la bomba auxiliar
+
+// Datos de la bomba auxiliar
 float vc_two;
 float vl_two;
 float nc_two;
@@ -327,34 +326,56 @@ void loop()
     // Este condicional es cuando el tanque está casi vacío (nivel bajo)
     if ((distance_one >= 18) && (distance_one <= 20))
     {
+      Serial.print("El tanque principal está en nivel bajo");
       digitalWrite(led_low_one, HIGH);
       digitalWrite(led_middle_one, LOW);
       digitalWrite(led_full_one, LOW);
-      digitalWrite(Bomba_Entrada, LOW);
-      digitalWrite(Bomba_Salida, HIGH);
+      // Este condicional verifica si el tanque auxiliar tiene suficiente agua para el principal, si la tiene entonces se activa la bomba principal
+      if ((distance_two >= 5) && (distance_two <= 18))
+      {
+        digitalWrite(Bomba_Entrada, HIGH);
+      }
     }
     // Este condicional es cuando el tanque está en nivel medio
     if ((distance_one >= 11) && (distance_one <= 13))
     {
+      Serial.print("El tanque principal está en nivel medio");
       digitalWrite(led_low_one, LOW);
       digitalWrite(led_middle_one, HIGH);
       digitalWrite(led_full_one, LOW);
+      // Este condicional verifica si el tanque auxiliar tiene suficiente agua para el principal, si la tiene entonces se activa la bomba principal
+      if ((distance_two >= 5) && (distance_two <= 18))
+      {
+        digitalWrite(Bomba_Entrada, HIGH);
+      }
+      // Este condicional es para activar la bomba de salida en caso de que la humedad de la planta lo requiera
+      if (mp == 20)
+      {
+        digitalWrite(Bomba_Salida, HIGH);
+        Serial.print("La bomba de salida está activada...");
+      }
     }
     // Este condicional es cuando el tanque está casi lleno (nivel alto)
     if ((distance_one >= 5) && (distance_one <= 7))
     {
+      Serial.print("El tanque principal está en nivel alto");
       digitalWrite(led_low_one, LOW);
       digitalWrite(led_middle_one, LOW);
       digitalWrite(led_full_one, HIGH);
-      digitalWrite(Bomba_Entrada, HIGH);
-      digitalWrite(Bomba_Salida, LOW);
+      // Este condicional es para activar la bomba de salida en caso de que la humedad de la planta lo requiera
+      if (mp == 20)
+      {
+        digitalWrite(Bomba_Entrada, LOW);
+        digitalWrite(Bomba_Salida, HIGH);
+        Serial.print("La bomba de salida está activada...");
+      }
     }
     //----------------------- Estado del Tanque Principal ----------------------
 
     // Nivel bajo
     if (distance_one > 15)
     {
-      client.publish(topic15, "El tanque principal está en nivel bajo");
+      client.publish(topic7, "Nivel bajo");
       client.publish(topic9, vl_one_String);
       client.publish(topic11, NTl_one_String);
       client.publish(topic13, c_one_String);
@@ -362,7 +383,7 @@ void loop()
     // Nivel medio
     if ((distance_one >= 9) && (distance_one <= 15))
     {
-      client.publish(topic15, "El tanque principal está en nivel medio");
+      client.publish(topic7, "Nivel medio");
       client.publish(topic9, vl_one_String);
       client.publish(topic11, NTl_one_String);
       client.publish(topic13, c_one_String);
@@ -370,7 +391,7 @@ void loop()
     // Nivel alto
     if ((distance_one < 9))
     {
-      client.publish(topic15, "El tanque principal está en nivel alto");
+      client.publish(topic7, "Nivel alto");
       client.publish(topic9, vl_one_String);
       client.publish(topic11, NTl_one_String);
       client.publish(topic13, c_one_String);
@@ -402,8 +423,7 @@ void loop()
       digitalWrite(led_low_two, HIGH);
       digitalWrite(led_middle_two, LOW);
       digitalWrite(led_full_two, LOW);
-      digitalWrite(Bomba_Entrada, LOW);
-      digitalWrite(Bomba_Salida, HIGH);
+      Serial.print("El tanque auxiliar está casi vacío, por favor llenar.");
     }
     // Este condicional es cuando el tanque está en nivel medio
     if ((distance_two >= 11) && (distance_two <= 13))
@@ -411,6 +431,13 @@ void loop()
       digitalWrite(led_low_two, LOW);
       digitalWrite(led_middle_two, HIGH);
       digitalWrite(led_full_two, LOW);
+      Serial.print("El tanque auxiliar está en nivel medio");
+      // Este condicional es para activar la bomba de entrada en caso de que el tanque principal esté en nivel medio o inferior
+      if (distance_one < 13)
+      {
+        digitalWrite(Bomba_Entrada, HIGH);
+        Serial.print("Entregando agua al tanque principal");
+      }
     }
     // Este condicional es cuando el tanque está casi lleno (nivel alto)
     if ((distance_two >= 5) && (distance_two <= 7))
@@ -418,8 +445,13 @@ void loop()
       digitalWrite(led_low_two, LOW);
       digitalWrite(led_middle_two, LOW);
       digitalWrite(led_full_two, HIGH);
-      digitalWrite(Bomba_Entrada, HIGH);
-      digitalWrite(Bomba_Salida, LOW);
+      Serial.print("El tanque auxiliar está en nivel alto");
+      // Este condicional es para activar la bomba de entrada en caso de que el tanque principal esté en nivel medio o inferior
+      if (distance_one < 13)
+      {
+        digitalWrite(Bomba_Entrada, HIGH);
+        Serial.print("Entregando agua al tanque principal");
+      }
     }
 
     //----------------------- Estado del Tanque Auxiliar ----------------------
@@ -427,7 +459,7 @@ void loop()
     // Nivel bajo
     if (distance_two > 15)
     {
-      client.publish(topic16, "El tanque auxiliar está en nivel bajo");
+      client.publish(topic8, "Nivel bajo");
       client.publish(topic10, vl_two_String);
       client.publish(topic12, NTl_two_String);
       client.publish(topic14, c_two_String);
@@ -435,7 +467,7 @@ void loop()
     // Nivel medio
     if ((distance_two >= 9) && (distance_two <= 15))
     {
-      client.publish(topic16, "El tanque auxiliar está en nivel medio");
+      client.publish(topic8, "Nivel medio");
       client.publish(topic10, vl_two_String);
       client.publish(topic12, NTl_two_String);
       client.publish(topic14, c_two_String);
@@ -443,7 +475,7 @@ void loop()
     // Nivel alto
     if ((distance_two < 9))
     {
-      client.publish(topic16, "El tanque auxiliar está en nivel alto");
+      client.publish(topic8, "Nivel alto");
       client.publish(topic10, vl_two_String);
       client.publish(topic12, NTl_two_String);
       client.publish(topic14, c_two_String);
